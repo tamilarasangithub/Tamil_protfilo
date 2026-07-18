0af4de47d59894b9e1b70a74db1a1abffc26a755
TAMILARASAN <151041799+tamilarasangithub@users.noreply.github.com>
2026-07-18 07:02:36 +0530

    Completely unclip card slider boundaries and fix button hover shadows

--- src/components/FeaturedSection.jsx
+++ src/components/FeaturedSection.jsx
@@ -14,7 +14,7 @@ export const FeaturedSection = ({ projects, certifications }) => {
   };
 
   return (
-    <div className="w-full overflow-hidden">
+    <div className="w-full">
       <div className="w-full">
         
         {/* SECTION 1: PROJECTS */}
@@ -39,10 +39,10 @@ export const FeaturedSection = ({ projects, certifications }) => {
           </div>
 
           {/* Bound Viewport Wrapper */}
-          <div className="overflow-hidden -my-4">
+          <div className="-my-6 -mx-2">
             <div 
               ref={projectScrollRef}
-              className="flex flex-row gap-6 w-full overflow-x-auto snap-x snap-mandatory scroll-smooth scrollbar-none py-4 px-2"
+              className="flex flex-row gap-6 w-full overflow-x-auto snap-x snap-mandatory scroll-smooth scrollbar-none py-6 px-4"
             >
               {projects.map((project, index) => (
                 <article 
@@ -83,10 +83,10 @@ export const FeaturedSection = ({ projects, certifications }) => {
           </div>
 
           {/* Bound Viewport Wrapper */}
-          <div className="overflow-hidden -my-4">
+          <div className="-my-6 -mx-2">
             <div 
               ref={certScrollRef}
-              className="flex flex-row gap-6 w-full overflow-x-auto snap-x snap-mandatory scroll-smooth scrollbar-none py-4 px-2"
+              className="flex flex-row gap-6 w-full overflow-x-auto snap-x snap-mandatory scroll-smooth scrollbar-none py-6 px-4"
             >
               {certifications.map((cert, index) => (
                 <article 
